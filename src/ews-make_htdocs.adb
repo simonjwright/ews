323faa6b9705e0657184f3b07323445954c4db10
Simon Wright <simonjwright@users.sourceforge.net>
2013-10-15 18:22:30 +0100

    Source updated to GPL v3 (with runtime exception where applicable).
      * lib/HttpInteraction.js, src/ews-dynamic.adb, src/ews-dynamic.ads,
        src/ews-http-ews_attachments_friend.adb,
        src/ews-http-ews_attachments_friend.ads, src/ews-http.adb,
        src/ews-http.ads, src/ews-reference_counted_pointers_g.adb,
        src/ews-reference_counted_pointers_g.ads, src/ews-server.adb,
        src/ews-server.ads, src/ews-static.adb, src/ews-static.ads,
        src/ews-types.adb, src/ews-types.ads, src/ews.ads,
        src/input_sources-ews_attachments.adb,
        src/input_sources-ews_attachments.ads: updated to GPL v3 with
          runtime exception.
      * src/ews-make_htdocs.adb: updated to GPL v3.

--- src/ews-make_htdocs.adb
+++ src/ews-make_htdocs.adb
@@ -1,16 +1,16 @@
---  Copyright (C) Simon Wright <simon@pushface.org>
-
 --  This package is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
---  published by the Free Software Foundation; either version 2, or
---  (at your option) any later version. This package is distributed in
---  the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+--  published by the Free Software Foundation; either version 3, or
+--  (at your option) any later version.  It is distributed in the
+--  hope that it will be useful, but WITHOUT ANY WARRANTY; without
 --  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
---  PARTICULAR PURPOSE. See the GNU General Public License for more
---  details. You should have received a copy of the GNU General Public
---  License distributed with this package; see file COPYING.  If not,
---  write to the Free Software Foundation, 59 Temple Place - Suite
---  330, Boston, MA 02111-1307, USA.
+--  PARTICULAR PURPOSE.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; see the file COPYING3. If not, see
+--  <http://www.gnu.org/licenses/>.
+--
+--  Copyright Simon Wright <simon@pushface.org>
 
 with Ada.Command_Line;
 with Ada.Streams.Stream_IO;
